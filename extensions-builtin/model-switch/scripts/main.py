d6272c43707445b1b41bb75191e4a23dd8267554
rkfg <rkfg@rkfg.me>
2023-04-29 18:39:13 +0300

    Add typing

--- extensions-builtin/model-switch/scripts/main.py
+++ extensions-builtin/model-switch/scripts/main.py
@@ -1,19 +1,21 @@
 from modules import extra_networks, script_callbacks, sd_models
+from modules.processing import StableDiffusionProcessing
 from modules.shared import opts
 
 class ExtraNetworkModel(extra_networks.ExtraNetwork):
     def __init__(self):
         super().__init__('model')
 
-    def activate(self, p, params_list):
+    def activate(self, p: StableDiffusionProcessing, params_list):
         if len(params_list) == 0:
             self.model = None
             return
         self.model = opts.sd_model_checkpoint
         opts.sd_model_checkpoint = params_list[0].items[0]
+
         sd_models.reload_model_weights()
 
-    def deactivate(self, p):
+    def deactivate(self, p: StableDiffusionProcessing):
         if not self.model:
             return
         opts.sd_model_checkpoint = self.model
