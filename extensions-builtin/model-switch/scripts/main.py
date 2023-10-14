7e012c7adcfca1d3623a7931ae6f73213de674a1
rkfg <rkfg@rkfg.me>
2023-10-14 19:09:40 +0300

    Fix generation data for model override

--- extensions-builtin/model-switch/scripts/main.py
+++ extensions-builtin/model-switch/scripts/main.py
@@ -15,6 +15,9 @@ class ExtraNetworkModel(extra_networks.ExtraNetwork):
             return
         opts.sd_model_checkpoint = params_list[0].items[0]
         sd_models.reload_model_weights()
+        p.extra_generation_params["Model hash"] = params_list[0].items[0]
+        if len(params_list[0].items) > 1:
+            p.extra_generation_params["Model"] = params_list[0].items[1]
 
     def deactivate(self, p: StableDiffusionProcessing):
         if self.model is None:
@@ -32,6 +35,7 @@ class ExtraNetworkClipSkip(extra_networks.ExtraNetwork):
             return
 
         opts.CLIP_stop_at_last_layers = int(params_list[0].items[0])
+        p.extra_generation_params["Clip skip"] = opts.CLIP_stop_at_last_layers
 
     def deactivate(self, p: StableDiffusionProcessing):
         opts.CLIP_stop_at_last_layers = self.cs
