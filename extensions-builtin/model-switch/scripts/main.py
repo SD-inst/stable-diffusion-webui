89cedc471ad6300ef63b58a093685b9177b358f7
rkfg <rkfg@rkfg.me>
2023-10-15 12:31:39 +0300

    Remove model/cs tags from the prompt

--- extensions-builtin/model-switch/scripts/main.py
+++ extensions-builtin/model-switch/scripts/main.py
@@ -1,6 +1,10 @@
 from modules import extra_networks, script_callbacks, sd_models
 from modules.processing import StableDiffusionProcessing
 from modules.shared import opts
+import re
+
+modelPromptCleaner = re.compile('<model:[^>]*>\s*', re.I)
+csPromptCleaner = re.compile('<cs:[^>]*>\s*', re.I)
 
 class ExtraNetworkModel(extra_networks.ExtraNetwork):
     def __init__(self):
@@ -15,6 +19,8 @@ class ExtraNetworkModel(extra_networks.ExtraNetwork):
             return
         opts.sd_model_checkpoint = params_list[0].items[0]
         sd_models.reload_model_weights()
+        p.all_prompts = [modelPromptCleaner.sub('', pr) for pr in p.all_prompts]
+        p.main_prompt = modelPromptCleaner.sub('', p.main_prompt)
         p.extra_generation_params["Model hash"] = params_list[0].items[0]
         if len(params_list[0].items) > 1:
             p.extra_generation_params["Model"] = params_list[0].items[1]
@@ -36,6 +42,8 @@ class ExtraNetworkClipSkip(extra_networks.ExtraNetwork):
 
         opts.CLIP_stop_at_last_layers = int(params_list[0].items[0])
         p.extra_generation_params["Clip skip"] = opts.CLIP_stop_at_last_layers
+        p.all_prompts = [csPromptCleaner.sub('', pr) for pr in p.all_prompts]
+        p.main_prompt = csPromptCleaner.sub('', p.main_prompt)
 
     def deactivate(self, p: StableDiffusionProcessing):
         opts.CLIP_stop_at_last_layers = self.cs
